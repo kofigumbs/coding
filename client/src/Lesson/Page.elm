c6c1f4301c104bc2fd332940e748d6c1b71ded7b
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-03-31 09:57:55 -0500

    Add text lession item type

--- client/src/Lesson/Page.elm
+++ client/src/Lesson/Page.elm
@@ -1,21 +1,36 @@
 module Lesson.Page exposing (Model, Msg, init, update, view)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Palette
 import Task exposing (Task)
 
 
 type alias Model =
     { code : String
+    , items : List Item
     }
 
 
+type Item
+    = Text String
+
+
 type Msg
-    = NoOp
+    = JumpTo Int
 
 
 init : String -> Task Never Model
 init code =
-    Task.succeed { code = code }
+    Task.succeed
+        { code = code
+        , items =
+            [ Text "Hello, World"
+            , Text "Oh, hi there?"
+            , Text "Bye bye."
+            ]
+        }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -24,5 +39,73 @@ update msg model =
 
 
 view : Model -> Html Msg
-view { code } =
-    text <| "<CHAPTER " ++ code ++ " COMING SOON...>"
+view model =
+    div
+        [ style
+            [ ( "display", "flex" )
+            , ( "flex-direction", "column" )
+            , ( "height", "100vh" )
+            , ( "width", "100vw" )
+            ]
+        ]
+        [ div
+            [ style [ ( "flex", "1" ) ]
+            ]
+            []
+        , nav
+            [ style
+                [ ( "display", "flex" )
+                , ( "flex-direction", "row" )
+                , ( "align-items", "center" )
+                , ( "justify-content", "center" )
+                , ( "width", "100vw" )
+                , ( "height", "65px" )
+                , ( "border-top", "2px " ++ Palette.light ++ " solid" )
+                ]
+            ]
+          <|
+            List.concat
+                [ [ a
+                        [ class "link"
+                        , style [ ( "color", Palette.primary ) ]
+                        ]
+                        [ text "Previous" ]
+                  ]
+                , List.indexedMap viewProgress model.items
+                , [ a
+                        [ class "link"
+                        , style
+                            [ ( "background-color", Palette.primary )
+                            , ( "color", Palette.invert Palette.primary )
+                            ]
+                        ]
+                        [ text "Next" ]
+                  ]
+                ]
+        ]
+
+
+viewProgress : Int -> Item -> Html Msg
+viewProgress index item =
+    let
+        clickable =
+            if False then
+                [ style
+                    [ ( "color", Palette.primary )
+                    ]
+                , onClick <| JumpTo index
+                ]
+            else
+                [ style [ ( "color", Palette.light ) ]
+                , disabled True
+                ]
+    in
+    a
+        (List.concat
+            [ clickable
+            , [ class "link"
+              , style [ ( "margin", "0 15px" ) ]
+              ]
+            ]
+        )
+        [ text "○" ]
