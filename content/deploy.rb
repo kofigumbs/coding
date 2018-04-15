81b976e1d00546961283e9d05e854a4b459fa810
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-04-15 14:19:30 -0500

    Cosmetic

--- content/deploy.rb
+++ content/deploy.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require "json"
 require "yaml"
 require "fileutils"
@@ -10,7 +12,7 @@ YAML.load_file(File.join(content, "_data", "lessons.yaml")).each do |config|
   File.write File.join(destination, config["location"]), JSON.generate(
     title: config["title"],
     items: config["items"].map do |item|
-      markdown = File.join(content, config["location"], "#{item}.md")
+      markdown = File.join content, config["location"], "#{item}.md"
       YAML.load_file(markdown).tap do |data|
         data["content"] = File.read(markdown).split("---")[2..-1].join.strip
       end
