ff8b2cef8f80fad13e07c78b3c16e5ef201b7630
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-04-15 14:08:06 -0500

    Make lesson config more generic

--- content/deploy.rb
+++ content/deploy.rb
@@ -6,13 +6,11 @@ content = File.dirname(__FILE__)
 destination = File.join File.dirname(content), "client", "public", "api", "lessons"
 FileUtils.mkdir_p destination
 
-Dir.glob File.join(content, "_data", "*.yaml") do |path|
-  slug = File.basename(path, ".yaml")
-  config = YAML.load_file(path)
-  File.write File.join(destination, slug), JSON.generate(
+YAML.load_file(File.join(content, "_data", "lessons.yaml")).each do |config|
+  File.write File.join(destination, config["location"]), JSON.generate(
     title: config["title"],
     items: config["items"].map do |item|
-      markdown = File.join(content, slug, "#{item}.md")
+      markdown = File.join(content, config["location"], "#{item}.md")
       YAML.load_file(markdown).tap do |data|
         data["content"] = File.read(markdown).split("---")[2..-1].join.strip
       end
