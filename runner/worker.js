4115db4f78533e5028d85bfa9074d9dfce2409bc
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-05-06 13:15:46 -0500

    Add bulma to compiled output

--- runner/worker.js
+++ runner/worker.js
@@ -5,14 +5,27 @@ const util = require("util");
 
 const shell = x => util.promisify(proc.exec)(x).then(y => y.stdout.trim());
 
+const toHtml = js => `
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    <script type="text/javascript">${js}</script>
+  </head>
+  <body></body>
+  <script type="text/javascript">Elm.Main.fullscreen();</script>
+</html>
+`;
+
 exports.handler = async (body, callback) => {
   const input = await shell("mktemp");
   const output = await shell("mktemp");
   await util.promisify(fs.writeFile)(input, body.elm);
   try {
-    await shell(`elm-make --yes --output=${output}.html ${input}`);
-    const html = await util.promisify(fs.readFile)(`${output}.html`, "utf8");
-    callback({ output: html });
+    await shell(`elm-make --yes --output=${output}.js ${input}`);
+    const js = await util.promisify(fs.readFile)(`${output}.js`, "utf8");
+    callback({ output: toHtml(js) });
   } catch(e) {
     callback({ error: e.stderr.split(input).join("") });
   }
