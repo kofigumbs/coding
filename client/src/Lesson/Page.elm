22e3d0d9b47775f7c447914d2773f341f4a6d4f0
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-07-07 19:20:26 -0500

    Fixup the example lesson

--- client/src/Lesson/Page.elm
+++ client/src/Lesson/Page.elm
@@ -53,13 +53,45 @@ init : Global.Context -> String -> Task Never Model
 init context slug =
     -- Http.getString ("%PUBLIC_PATH%/lessons/" ++ slug ++ ".md")
     -- |> Http.toTask
-    Task.succeed """# Guess the secret number
+    Task.succeed """
+# Welcome
+
+> Spreadsheets are so usable because they put the data right in front of you...
+> Spreadsheets show the data and hide the functionality.
+> Code shows the functionality, but hides the data.
+>
+> — _citation needed_
+
+In this course, we'll do our best to present both the functionality _and_ the data.
+For example, in the snippet below, you can change the **code on the left**,
+and see the **change reflected immediately on the right**.
+Go ahead and try typing your name into the program!
+
+```elm
+import Essentials exposing (table, row2)
+
+main =
+  table
+    [ row2 "Hello" "__YOUR NAME HERE__"
+    ]
+```
+
+# A quick guessing game
+
+Now that you know how to make changes, let's get right to our first challenge.
+This course is structured around short challenges that give you a taste of how programming works.
+The challenges are short, but they still require effort.
+Most of them are based off real world problems.
+
+This first one is easy — just to get you in the habit of typing into the editor.
+There is a `secretNumber` that we need you to guess.
+Here's a hint: it's between 1 and 10.
 
 ```elm
 import Essentials exposing (table, row2, secretNumber)
 
 guess =
-  7
+  0
 
 main =
   table
