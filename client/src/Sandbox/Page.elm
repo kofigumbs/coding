b396c4f7665fae55d7a2f64c0aa8e675a687cfba
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-07-22 14:22:26 -0500

    Use editor in sandbox

--- client/src/Sandbox/Page.elm
+++ client/src/Sandbox/Page.elm
@@ -1,32 +1,57 @@
 module Sandbox.Page exposing (Model, Msg, init, subscriptions, update, view)
 
+import Editor.View
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Js
 
 
 type alias Model =
-    {}
+    { editor : Editor.View.State
+    }
 
 
 init : Js.Flags -> ( Model, Cmd Msg )
 init flags =
-    ( {}, Cmd.none )
+    applyEdits <| Editor.View.init flags 0 "someVariable = \"Hello, World!\""
 
 
-type alias Msg =
-    ()
+type Msg
+    = EditorMsg Editor.View.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update () model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        EditorMsg childMsg ->
+            applyEdits <| Editor.View.update childMsg model.editor
+
+
+applyEdits : ( Editor.View.State, Cmd Editor.View.Msg ) -> ( Model, Cmd Msg )
+applyEdits ( editor, cmds ) =
+    ( Model editor, Cmd.map EditorMsg cmds )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.map EditorMsg <| Editor.View.subscriptions model.editor
 
 
 view : Model -> Html Msg
 view model =
-    Html.text "Sandbox"
+    div []
+        [ div
+            [ class "hero" ]
+            [ div
+                [ class "hero-body" ]
+                [ h1 [ class "title" ] [ text "Your sandbox" ]
+                , h2 [ class "subtitle" ] [ text """
+                    Play around as much as you want!
+                    All of your changes are stored locally, on your computer.
+                  """ ]
+                ]
+            ]
+        , div
+            [ style [ ( "height", "100vh" ) ] ]
+            [ Html.map EditorMsg <| Editor.View.view model.editor ]
+        ]
